7fac9117ebb43078e2f2517c5278ad04ff9cae33
Vsevolod Volgin <volginvs@list.ru>
2020-11-28 16:33:59 +0300

    Master3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello World!!");
         System.out.println("Master branch");
         System.out.println("Master branch: 1111");
+        System.out.println("Master branch: 2222");
     }
 }
